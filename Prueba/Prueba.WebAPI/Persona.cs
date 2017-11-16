025453fffc3e2a1602d47f11c473144ddc37b1dd
patricia.jimenezball <patricia.jimenezball@ES-L002068>
2017-11-16 09:16:00 +0100

    Clase Persona

---
+++ Prueba/Prueba.WebAPI/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba.WebAPI
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public int Edad(DateTime fechaNacimiento)
+        {
+            var resultado = DateTime.Today.Year - fechaNacimiento.Year;
+            return resultado;
+        }
+    }
+}
