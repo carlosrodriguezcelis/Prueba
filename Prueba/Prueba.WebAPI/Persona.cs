ec9b906b2a8f6a64d939dfb2e8f555f8fcf0e5a9
Carlos Rodríguez Celis <carlos.rodriguezcelis@outlook.com>
2017-11-16 09:25:17 +0100

    Direccion

--- Prueba/Prueba.WebAPI/Persona.cs
+++ Prueba/Prueba.WebAPI/Persona.cs
@@ -11,6 +11,7 @@ namespace Prueba.WebAPI
 
         public string apellido { get; set; }
 
+        public string direccion { get; set; }
         public int Edad(DateTime fechaNacimiento)
         {
             var resultado = DateTime.Today.Year - fechaNacimiento.Year;
